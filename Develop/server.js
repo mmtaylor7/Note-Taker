9ae80abf5c8dfd5fba1c3970980407dc668f7d53
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-07 11:56:03 -0400

    added notes on server.js

--- Develop/server.js
+++ Develop/server.js
@@ -10,16 +10,17 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
+//get for index.html main page
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
-
+//get for notes.html notetaker page
 app.get("/notes", (req, res) => {
   console.log(__dirname);
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+//app is listening on port variable 4200 defined at top
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
