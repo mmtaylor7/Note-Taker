4132dfc67db7b3e5b2815112b2625cb8420a37d2
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-07 11:06:14 -0400

    variabls in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,8 @@
-const { request } = require("express");
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const router = require("express").Router();
+
+const PORT = provess.env.PORT || 3001;
+
+const app = express();
