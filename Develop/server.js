b15ce583fba58f6f6390406b62612dd84acc3fad
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-07 11:52:08 -0400

    server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { get } = require("http");
 const router = require("express").Router();
 
-const PORT = provess.env.PORT || 3001;
+const PORT = 4200;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) => {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}`));
